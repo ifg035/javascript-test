d6f69b8f1c868c74c665a0537a7aec85bd36227a
Lim yeeun <ifg.035@daum.net>
2024-06-24 14:06:43 +0900

    javascript true false

--- script/main.js
+++ script/main.js
@@ -1,10 +1,29 @@
-function q2(){
-    let email = $('#input-q2').val();
-    if(email.includes('@')){
-        let damatunWithDot = email.split('@')[1];
-        let onlyDomain = domainWithDot.split('.')[0];
-        alert(onlyDomain);
-    } else {
-        alert('이메일이 아닙니다.');
-    }
-}
+//문자형
+// let value = true;
+// alert(typeof value); // boolean
+
+// value = String(value); // 변수 value엔 문자열 "true" 저장
+// alert(typeof value); // string
+
+//숫자형
+// alert("6"/"2");//3,문자열이 숫자형으로 자동변환된 후 연산이 수행
+
+// let str = "123";
+// alert(typeof str);
+// let num = Number(str); //문자열 "123"이 숫자 123으로 변환됩니다.
+// alert(typeof num); //number
+
+// let age = Number("임의의 문자열 123");
+// alert(age); //NaN,형 변환이 실패
+
+// alert( Number("  123  ")); //123
+// alert( Number("123z"));    //NaN ("z"를 숫자로 변환하는 데 실패함)
+// alert( Number(true));      //1
+// alert( Number(false));     //0
+
+//불린형
+// alert( Boolean(1) ); //숫자 1(true)
+// alert( Boolean(0) ); //숫자 0(false)
+
+// alert( Boolean("hello") ); //문자열(true)
+// alert( Boolean("") ); //빈 문자열(false)
